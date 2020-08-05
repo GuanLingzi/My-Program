b180bccdf99cac847b954a1800bc8ad101b69efc
GuanLingzi <15757339816@163.com>
2020-08-05 17:04:23 +0800

    create a new file

---
+++ python/作业/12_素数的列表2.py
@@ -0,0 +1,27 @@
+# 创建空列表
+prime_list = []
+# 2判断不出，手动加入
+prime_list.append(2)
+# 判断素数
+for j in range(2, 101):
+    for i in range(2, j):
+        if j % i == 0:
+            break
+        elif (j - 1) == i:
+            # 如果此数是素数，将它加入列表
+            prime_list.append(j)
+# 去除含7的素数
+for i in range(len(prime_list)-1, -1, -1):
+    if '7' in str(prime_list[i]):
+        prime_list.remove(prime_list[i])
+# 打印列表
+print(prime_list)
+# 定义max,n
+m = len(prime_list)
+n = int(input("请输入你要访问第几个素数,共%d个：" % m))
+
+# 输出
+if 0 < n <= m:
+    print(prime_list[n - 1])
+else:
+    print("输入不合法，请重新输入")
