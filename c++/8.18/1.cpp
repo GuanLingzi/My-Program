06e403acceb88ee2dbd62523f5e4ccbfefae8131
GuanLingzi <15757339816@163.com>
2020-08-18 09:35:27 +0800

    --

--- c++/8.18/1.cpp
+++ c++/8.18/1.cpp
@@ -5,12 +5,12 @@ int main()
 	unsigned long long f1,f2,f3,i;
 	f1=1;
 	f2=f1;
-	cout<<f1<<" "<<f2<<" ";
+	cout<<f1<<endl<<f2<<endl;
 	for (i=3;i<=100;i++)
 	{
 		f3=f1+f2;
-		cout<<f3<<" ";
+		cout<<f3<<endl;
 		f1=f2;
 		f2=f3;
-	} 
+	}
 }
