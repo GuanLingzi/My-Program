a58bc9a876a7d285df6ce34a975fa83244560f73
GuanLingzi <15757339816@163.com>
2020-08-18 09:24:48 +0800

    --

--- c++/8.18/1.cpp
+++ c++/8.18/1.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 int main()
 {
-	cout<<"rapgod";
+	unsigned long long f1,f2,f3,i;
+	f1=1;
+	f2=f1;
+	cout<<f1<<" "<<f2<<" ";
+	for (i=3;i<=100;i++)
+	{
+		f3=f1+f2;
+		cout<<f3<<" ";
+		f1=f2;
+		f2=f3;
+	} 
 }
