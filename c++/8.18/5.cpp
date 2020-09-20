5debd019f77ea5ae58d8633991ad994673940274
GuanLingzi <15757339816@163.com>
2020-09-20 21:59:35 +0800

    --

--- c++/8.18/5.cpp
+++ c++/8.18/5.cpp
@@ -4,28 +4,28 @@
 using namespace std;
 int main()
 {
-	const int MAX=10;
+	const int MAX=4;
 	int m,n,countm,countn,countx;
 	countm=countn=0;
 	for (int i=0;i<MAX;i++)
 	{
+		do
+		{
+		  cout<<"请你出招：";
+		  cout<<"1.剪刀 2.石头 3.布"<<endl;
+	    cin>>n;
+	  }while(n<1||n>3);
 		m=rand()%3+1;
-		cout<<"请你出招：";
-		cout<<"1.剪刀 2.石头 3.布"<<endl;
-		cin>>n;
-		if (n<1||n>3)
-		  cout<<"请输入1-3，此局无效！"<<endl;
-		else
-		  switch ((m-n+3)%3)
-		  {
-		  	case 1:cout<<"计算机赢！"<<endl; countm++; break;
-		  	case 0:cout<<"平局！"<<endl; countx++; break;
-		  	case -1:cout<<"你赢！"<<endl; countn++; break;
-			}
+	  switch ((m-n+3)%3)
+	  {
+  	  case 1:cout<<"计算机赢！"<<endl; countm++; break;
+  	  case 0:cout<<"平局！"<<endl; countx++; break;
+  	  default:cout<<"你赢！"<<endl; countn++; break;
+		}
 	}
 	cout<<endl;
 	cout<<"--------------------------------"<<endl;
 	cout<<"|"<<"计算机赢："<<countm<<"                   "<<"|"<<endl;
 	cout<<"|"<<"你赢："<<countn<<"                       "<<"|"<<endl;
-	cout<<"|"<<"平局及无效："<<countx<<"                 "<<"|";
+	cout<<"|"<<"平局："<<countx<<"                       "<<"|";
 } 
---
+++ python/作业/14_学生管理系统1.8.2.py
@@ -0,0 +1,77 @@
+# Student Achievement Management System
+name_list = []
+main = """
+===================================
+Welcome to [Student Achievement Management System] v1.8.2
+
+1: Add student information
+2: Delete student information
+3: Query all student information
+4: Find grades based on student names
+5: Modify the grades according to the student's name
+
+0: exit
+===================================="""
+exit_title = """
+================
+Thanks for used this system
+The system was exited
+================"""
+while True:
+    print(main)
+    a = int(input("Please write down a number："))
+    if a == 1:
+        name = input('name:')
+        gender = input('gender:')
+        score = input('score:')
+        person = (name, gender, score)
+        name_list.append(person)
+    elif a == 2:
+        find = input('delete who:')
+        i = 0
+        for name in name_list:
+            if name[0] == find:
+                k = input(f"is this person?：{name}('y' or other word):")
+                if k == 'y':
+                    name_list.pop(i)
+                    print('deleted successfully!')
+                else:
+                    print('continue to find next one')
+            i += 1
+    elif a == 3:
+        for name in name_list:
+            print(f"name:{name[0]} gender:{name[1]} score:{name[2]}")
+    elif a == 4:
+        find = input('find who:')
+        i = 0
+        for name in name_list:
+            if name[0] == find:
+                k = input(f"is this person?：{name}('y' or other word):")
+                if k == 'y':
+                    print(f"name:{name[0]} gender:{name[1]} score:{name[2]}")
+                else:
+                    print('continue to find next one')
+            i += 1
+    elif a == 5:
+        find = input('change who:')
+        i = 0
+        for name in name_list:
+            if name[0] == find:
+                k = input(f"is this person?：{name}('y' or other word):")
+                if k == 'y':
+                    print(f"name:{name[0]} gender:{name[1]} score:{name[2]}")
+                    name_list[i] = list(name)
+                    name = name_list[i]
+                    name[0] = input('new name:')
+                    name[1] = input('new gender:')
+                    name[2] = input('new score:')
+                    name_list[i] = tuple(name_list)
+                    print('changed successfully')
+                else:
+                    print('continue to find next one')
+            i += 1
+    elif a == 0:
+        print(exit_title)
+        break
+    else:
+        print("wrote is not support,try again")
