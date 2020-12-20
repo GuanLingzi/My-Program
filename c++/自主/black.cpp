c241b389aac51006c5a3769e93813fc476f5a07d
guanlingzi <15757339816@163.com>
2020-12-20 08:26:17 +0800

    --

--- c++/自主/black.cpp
+++ c++/自主/black.cpp
@@ -4,7 +4,17 @@
 
 #include<iostream>
 using namespace std;
+void show(int count)
+{
+    for (int i=0;i<count;i++)
+        cout<<"*";
+    cout<<endl;
+}
 int main()
 {
-    cout<<"Hello World";
+    int n=4;
+    show(1);
+    show(2);
+    show(n-1);
+    show(n);
 }
