e3d3e73319efe56a5db65c84aea1f2a9b7cc3996
guanlingzi <15757339816@163.com>
2021-01-09 20:53:42 +0800

    --

--- python/自主/blank.py
+++ python/自主/blank.py
@@ -2,14 +2,23 @@
 import time
 import random
 
-# Start the timing
-t0 = time.time()
-
 # Place your code here
-n = random.randint(1000000, 100000000)
 d = {}
 res = []
 nums = []
+
+result = input("Do you want to customize the length？（'y' or 'n'):")
+if result == "y":
+    n = int(input("Please input a integer:"))
+    print("The length is:", n, "\n")
+    time.sleep(2)
+elif result == 'n':
+    print("The length will be randomly selected between 1000000 and 100000000.\n")
+    n = random.randint(1000000, 100000000)
+
+# Start the timing
+t0 = time.time()
+
 target = random.randint(1, n)
 for n in range(1, n + 1):
     nums.append(n)
@@ -21,8 +30,10 @@ for idx, i in enumerate(nums):
         break
     else:
         d[diff] = idx
-print(res)
-print("Used length:", n)
+print("Index of the same value is:", res)
+
+if result == 'n':
+    print("Used length:", n)
 
 # Stop the timing
 t1 = time.time()
