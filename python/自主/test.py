a1439ebf84224556fea6e7a846d73f1662a7155d
guanlingzi <15757339816@163.com>
2021-01-23 15:46:04 +0800

    --

--- python/自主/test.py
+++
@@ -1,8 +0,0 @@
-nums = input().split()
-target = int(input())
-
-for i in range(len(nums) - 1):
-    for j in range(i + 1, len(nums)):
-        if int(nums[i]) + int(nums[j]) == target:
-            print({i, j})
-            break
